133b6b8aefb73a9800e80abb8becf5efc9adec58
Android-Bot-Programmer <a.voevodin.it@yandex.ru>
2023-08-26 23:11:18 +0300

    Dispatchers IO for server request and database request

--- app/src/main/java/ru/vaa/testedapp/data/cameras/CamerasViewModel.kt
+++ app/src/main/java/ru/vaa/testedapp/data/cameras/CamerasViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.vaa.testedapp.data.remote.ApiService
 import ru.vaa.testedapp.repository.MongoRepository
@@ -38,28 +39,32 @@ class CamerasViewModel @Inject constructor(
     }
 
 
-    private suspend fun getCameras() {
-        loadProgress.value = true
-        val response = apiService.getCameras()
-        if (response?.success == true) {
-            response.data.cameras.forEach {
-                repository.insert(Camera().apply {
-                    name = it.name
-                    snapshot = it.snapshot
-                    favorites = it.favorites
-                    room = it.room
-                    rec = it.rec
-                })
+    private fun getCameras() {
+        viewModelScope.launch(Dispatchers.IO) {
+            loadProgress.value = true
+            val response = apiService.getCameras()
+            if (response?.success == true) {
+                response.data.cameras.forEach {
+                    repository.insert(Camera().apply {
+                        name = it.name
+                        snapshot = it.snapshot
+                        favorites = it.favorites
+                        room = it.room
+                        rec = it.rec
+                    })
+                }
+                loadProgress.value = false
+            } else {
+                loadProgress.value = false
+                Log.d(tag, "Error response")
             }
-            loadProgress.value = false
-        } else {
-            loadProgress.value = false
-            Log.d(tag, "Error response")
         }
     }
 
-    suspend fun clearAll() {
-        _listCameras.value = null
-        repository.clearAll()
+    fun clearAll() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _listCameras.value = null
+            repository.clearAll()
+        }
     }
 }
